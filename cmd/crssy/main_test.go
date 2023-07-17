dce0045b48681d09833862f3a2cff9eaf6ab7b15
YukiKonishi <g1953535@cc.kyoto-su.ac.jp>
2023-07-17 15:19:58 +0900

    最終変更

--- cmd/crssy/main_test.go
+++ cmd/crssy/main_test.go
@@ -6,6 +6,18 @@ func ignore_Example_Main() {
 	goMain([]string{})
 }
 
+func Example_Completion() {
+	goMain([]string{"./crssy", "--generate-completions"})
+	// Output:
+	//
+	//crssy [OPTIONS]
+	//	OPTIONS
+	//		<LOCATION>   	引数に場所を指定をし，出力で指定した場所の天気を返す．
+	//		-v, --version 			バージョンを表示し，修了します.
+	//		-h, --help 			このメッセージを表示し，修了します.
+	//		-w, --week <WEEK>   	    	出力で週の天気を返す.
+}
+
 func Test_Main(t *testing.T) {
 	if status := goMain([]string{"./crssy", "Gifu"}); status != 0 {
 		t.Error("Expected 0, got ", status)
